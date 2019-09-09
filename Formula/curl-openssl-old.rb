980b5bf1692e37193e61bef8268a690ce653263a
Henk Rehorst <henkrehorst@outlook.com>
2019-09-09 18:20:37 +0200

    rename formula

--- Formula/curl-openssl-old.rb
+++ Formula/curl-openssl-old.rb
@@ -1,4 +1,4 @@
-class CurlOpenssl < Formula
+class CurlOpensslOld < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.haxx.se/"
   url "https://curl.haxx.se/download/curl-7.65.3.tar.bz2"
